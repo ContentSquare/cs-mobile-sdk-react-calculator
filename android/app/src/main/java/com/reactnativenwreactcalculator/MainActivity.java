a207100df3025922c1b2fbd03e1bfde74c3652a0
Marius Constantin <marius.constantin@contentsquare.com>
2018-05-15 16:44:33 +0200

    Fix compile error

--- android/app/src/main/java/com/reactnativenwreactcalculator/MainActivity.java
+++ android/app/src/main/java/com/reactnativenwreactcalculator/MainActivity.java
@@ -13,12 +13,10 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
     protected String getMainComponentName() {
         return "nativeApp";
     }
 
-    @Override
     protected String getJSMainModuleName() {
       return "src/index.android";
     }
@@ -27,7 +25,6 @@ public class MainActivity extends ReactActivity {
      * Returns whether dev mode should be enabled.
      * This enables e.g. the dev menu.
      */
-    @Override
     protected boolean getUseDeveloperSupport() {
         return BuildConfig.DEBUG;
     }
@@ -36,7 +33,6 @@ public class MainActivity extends ReactActivity {
      * A list of packages used by the app. If the app uses additional views
      * or modules besides the default ones, add more packages here.
      */
-    @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage()
